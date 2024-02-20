e0ce01dd3c7a3b19fda87a7bf49c9f6fb2a0ee06
gmacarroe <gmacarroe@gmail.com>
2024-02-20 18:21:31 +0000

    Intent amb cercador prou be

--- array_receptes.js
+++ array_receptes.js
@@ -1,5 +1,35 @@
 var receptes = [
     {
+        titol: "Recepta 1: Orada a la papillota",
+        descripcio: "Descripció de la recepta 1",
+        imatge: "images/receptes/orada_papillota.jpeg"
+    },
+    {
+        titol: "Recepta 2: Pà amb formatge",
+        descripcio: "Descripció de la recepta 2",
+        imatge: "imatge2.jpg"
+    },
+    {
+        titol: "Recepta 3: Botifarra amb seques",
+        descripcio: "Descripció de la recepta 3",
+        imatge: "imatge3.jpg"
+    },
+    {
+        titol: "Recepta 4: Cigrons amb arròs",
+        descripcio: "Descripció de la recepta 4",
+        imatge: "imatge4.jpg"
+    },
+    {
+        titol: "Recepta 5: Llenties amb xoriç",
+        descripcio: "Descripció de la recepta 5",
+        imatge: "imatge5.jpg"
+    },
+    {
+        titol: "Recepta 6: Flam amb nata",
+        descripcio: "Descripció de la recepta 6",
+        imatge: "imatge6.jpg"
+    },
+    {
         titol: "Recepta 1: Orada a la planxa",
         descripcio: "Descripció de la recepta 1",
         imatge: "imatge1.jpg"
@@ -13,6 +43,21 @@ var receptes = [
         titol: "Recepta 3: Botifarra amb seques",
         descripcio: "Descripció de la recepta 3",
         imatge: "imatge3.jpg"
+    },
+    {
+        titol: "Recepta 4: Cigrons amb arròs",
+        descripcio: "Descripció de la recepta 4",
+        imatge: "imatge4.jpg"
+    },
+    {
+        titol: "Recepta 5: Llenties amb xoriç",
+        descripcio: "Descripció de la recepta 5",
+        imatge: "imatge5.jpg"
+    },
+    {
+        titol: "Recepta 6: Flam amb nata",
+        descripcio: "Descripció de la recepta 6",
+        imatge: "imatge6.jpg"
     },
     // Més receptes...
 ];
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-    $('#searchInput').on('input', function() {
+$(document).ready(function () {
+    $('#searchInput').on('input', function () {
         var searchText = $(this).val().toLowerCase();
         $('#searchResults').empty();
         if (searchText.length >= 3) {
-            $('h3, p').each(function() {
+            $('h3, p').each(function () {
                 var text = $(this).text().toLowerCase();
                 if (text.includes(searchText)) {
                     $('#searchResults').append($(this).clone());
                 }
-            });          
+            });
         }
     });
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     mostrarReceptes();
 });
 
@@ -22,22 +22,21 @@ function mostrarReceptes() {
     var container = document.getElementById('contenidor_receptes');
     container.innerHTML = '';
 
-    receptes.forEach(function(recepta) {
+    receptes.forEach(function (recepta) {
+
         var receptaElement = document.createElement('div');
         receptaElement.classList.add('recepta');
 
+        var imageElement = document.createElement('img');
+        imageElement.src = recepta.imatge;
         var titleElement = document.createElement('h3');
         titleElement.textContent = recepta.titol;
-
         var descriptionElement = document.createElement('p');
         descriptionElement.textContent = recepta.descripcio;
 
-        var imageElement = document.createElement('img');
-        imageElement.src = recepta.imatge;
-
+        receptaElement.appendChild(imageElement);
         receptaElement.appendChild(titleElement);
         receptaElement.appendChild(descriptionElement);
-        receptaElement.appendChild(imageElement);
 
         container.appendChild(receptaElement);
     });
