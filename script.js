81620c0e811a4c6f9812bf1f3f54596ffcafabc8
gmacarroe <gmacarroe@gmail.com>
2024-02-24 11:01:02 +0000

    Cercador funciona a mitjes

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 $(document).ready(function () {
     $('#searchInput').on('input', function () {
         var searchText = $(this).val().toLowerCase();
-        $('#searchResults').empty();
+        $('#contenidor_receptes').empty();
         if (searchText.length >= 3) {
-            $('.recepta h3').each(function () {
-                var text = $(this).text().toLowerCase();
+            receptes.forEach(function (recepta) {
+                var text = recepta.titol.toLowerCase();
                 if (text.includes(searchText)) {
-                    $('#searchResults').append($(this).closest('.recepta').clone());
+                    var receptaElement = document.createElement('div');
+                    receptaElement.classList.add('recepta');
+
+                    var imageElement = document.createElement('img');
+                    imageElement.src = recepta.imatge;
+                    var titleElement = document.createElement('h3');
+                    titleElement.textContent = recepta.titol;
+
+                    receptaElement.appendChild(imageElement);
+                    receptaElement.appendChild(titleElement);
+
+                    $('#contenidor_receptes').append(receptaElement);
+
+                    // Afegeix un event listener al div de la recepta per a l'acció de clic
+                    receptaElement.addEventListener('click', function() {
+                        // Redirigeix a la pàgina HTML corresponent
+                        window.location.href = 'receptes/' + recepta.titol.toLowerCase().replace(/ /g, '_') + '.html';
+                    });
                 }
             });
+        } else {
+            mostrarReceptes();
         }
     });
 });
