52e7b7f983d42ea6dc68ef0b1dd311220171e4dc
gmacarroe <gmacarroe@gmail.com>
2024-02-26 19:23:01 +0000

    Prova mail php

--- enviar_contacte.php
+++ enviar_contacte.php
@@ -1,5 +1,23 @@
 <?php
-ini_set("SMTP", "smtp.lacuinadelbarbut.cat");
-ini_set("smtp_port", "587");
-ini_set("sendmail_from", "info@lacuinadelbarbut.cat");
+// Recollir les dades del formulari
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+// Configurar el destinatari i el cos del correu electrònic
+$to = "contacte.lacuinadelbarbut@gmail.com";
+$subject = "Missatge de contacte: " . $subject;
+$body = "Nom: " . $name . "\n";
+$body .= "Correu electrònic: " . $email . "\n";
+$body .= "Telèfon: " . $phone . "\n\n";
+$body .= "Missatge:\n" . $message;
+
+// Enviar el correu electrònic
+if (mail($to, $subject, $body)) {
+    echo "El teu missatge s'ha enviat amb èxit.";
+} else {
+    echo "Hi ha hagut un problema en enviar el missatge.";
+}
 ?>
