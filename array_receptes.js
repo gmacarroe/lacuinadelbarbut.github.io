c02bab9b77ee8ea52feb8e2b7a9b143813efeaa5
gmacarroe <gmacarroe@gmail.com>
2024-02-27 11:15:44 +0000

    Canvis imatges webp i eliminar fitxers duplicats

--- array_receptes.js
+++ array_receptes.js
@@ -1,35 +1,35 @@
 var receptes = [
     {
         titol: "Orada a la papillota",
-        imatge: "images/receptes/orada_papillota.jpeg"
+        imatge: "images/receptes/orada_papillota.webp"
     },
     {
         titol: "Porro escalivat amb salsa",
-        imatge: "images/receptes/porro_escalivat.jpeg"
+        imatge: "images/receptes/porro_escalivat.webp"
     },
     {
         titol: "Peus de porc amb nap negre",
-        imatge: "images/receptes/peusporc_napnegre.jpeg"
+        imatge: "images/receptes/peusporc_napnegre.webp"
     },
     {
         titol: "Crema de llimona",
-        imatge: "images/receptes/crema_llimona.jpeg"
+        imatge: "images/receptes/crema_llimona.webp"
     },
     {
         titol: "Conserva de carxofes",
-        imatge: "images/receptes/conserva_carxofes.jpeg"
+        imatge: "images/receptes/conserva_carxofes.webp"
     },
     {
         titol: "Olla aranesa",
-        imatge: "images/receptes/olla_aranesa.jpeg"
+        imatge: "images/receptes/olla_aranesa.webp"
     },
     {
         titol: "Panacotta de torró",
-        imatge: "images/receptes/panacotta_torro.jpeg"
+        imatge: "images/receptes/panacotta_torro.webp"
     },
     {
         titol: "Torradetes de Santa Teresa",
-        imatge: "images/receptes/torrades_teresa.jpeg"
+        imatge: "images/receptes/torrades_teresa.webp"
     },
     // Més receptes... 
 ];
